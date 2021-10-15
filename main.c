84481d1142eed2cba1cde8a67d593b454fdcd734
jrichard1909 <jrichard1909@gmail.com>
2021-10-14 22:51:38 -0700

    main.c fixed

--- main.c
+++ main.c
@@ -18,14 +18,14 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	var.file_p = fopen(argv[1], "r");
 	if (!var.file_p)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
